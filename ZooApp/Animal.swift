fb7fc8f9454654711ac50ed60c5a6d47f6273732
ema2497 <49290161+ema2497@users.noreply.github.com>
2019-04-04 14:34:41 -0600

    Add files via upload

---
+++ ZooApp/Animal.swift
@@ -0,0 +1,22 @@
+//
+//  Animal.swift
+//  ZooApp
+//
+//  Created by mcTI-SO on 01/04/19.
+//  Copyright © 2019 usuario. All rights reserved.
+//
+
+import UIKit
+
+class Animal {
+    var name:String?
+    var des:String?
+    var image:String?
+    
+    init(name:String,des:String,image:String){
+        self.name=name
+        self.des=des
+        self.image=image
+    }
+
+}
